58126dd93846b09d1eae7256e060a78ef2da3d7a
aslamnahas <nahasnazzz06@gmail.com>
2024-06-10 21:52:02 +0530

    midlleadd

--- dsa.py
+++ dsa.py
@@ -2,6 +2,7 @@ class Node:
     def __init__(self,data):
         self.data =data
         self.ref = None
+
 class Linked_list:
     def __init__(self):
         self.head=None
@@ -10,7 +11,7 @@ class Linked_list:
             print('linked list is empty')
         else:
             n=self.head
-            while n is not None:
+            while n is not None: 
                 print(n.data,'--->',end=" ")
                 n = n.ref
                 # print(n)
@@ -19,7 +20,6 @@ class Linked_list:
         new_noode = Node(data)
         new_noode.ref= self.head
         self.head = new_noode
-
     def end_add(self,data):
         newnode=Node(data)
         if self.head is None:
@@ -30,12 +30,96 @@ class Linked_list:
                 n=n.ref
             n.ref=newnode
 
+
+
+    def add_middle(self,data,x):
+        n=self.head
+        while n is not None:
+
+            if n.data==x:
+                break
+                
+            n= n.ref
+        if n is None:
+            print("node is not in this linked list")
+        else:
+            new_node =Node(data)
+            new_node.ref=n.ref
+            n.ref = new_node
+
+    def middlechck(self,data,x):
+        if self.head is None:
+            print('the list is empkty')
+        
+        if self.head.data==x:
+            newnode=Node(data)
+            newnode.ref=self.head
+            self.head=newnode
+            return
+        n=self.head
+        while n.ref is not None:
+            if n.ref.data==x:
+                break
+            n=n.ref
+        if n.ref is None:
+            print(f"Node with data {x} is not in the linked list")
+        else:
+            newnode=Node(data)
+            newnode.ref=n.ref
+            n.ref=newnode
+
+    def middleaddbefore(self,data,x):
+         if self.head is None:
+             print("List is empty")
+             return
+        
+         if self.head.data==x:
+            newnode=Node(data)
+            newnode.ref=self.head
+            self.head=newnode
+            return
+         n = self.head
+         while n.ref is not None:
+             if n.ref.data == x:
+                 break
+             n = n.ref
+         if n.ref is None:
+            print(f"Node with data {x} is not in the linked list")
+         else:
+            newnode=Node(data)
+            newnode.ref=n.ref
+            n.ref=newnode
+
+    def middle(self):
+        if self.head is None:
+            return None
+        slow = self.head
+        fast = self.head
+        while fast is not None and fast.ref is not None:
+            slow = slow.ref
+            fast = fast.ref.ref
+        return slow.data
+  
 ll1=Linked_list()
 # s=[1,2,3,4]
-ll1.add_first(10)
+
+# ll1.add_first(10)
+# for i in s:
+    # ll1.end_add(i)
 ll1.end_add(22)
-ll1.add_first(111)
-ll1.end_add(9)
-ll1.add_first(2)
+
+ll1.middlechck(15, 2)
 
 ll1.printt()
+ll1.add_first(111)
+ll1.end_add(9)
+ll1.add_middle(102,3)
+ll1.add_middle(102,3)
+ll1.add_first(2)
+# ll1.middle()
+ll1.printt()
+# print()
+ll1.middleaddbefore(100,2)
+ll1.printt()
+a = ll1.middle()
+print('midleee',a)
